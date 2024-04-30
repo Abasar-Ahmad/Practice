10358a63fe16062433adbe4c35e4b428be005fa1
Abasar-Ahmad <absarbhu2015@gmail.com>
2024-04-30 11:01:44 +0530

    First Comment of Life

---
+++ src/Learn.java
@@ -0,0 +1,10 @@
+
+import  java.util.Scanner;
+public class Learn{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Inter input to print");
+        String  ab = sc.nextLine(); //  is 2 statement ke vajah se ham jitna likhainge sb print ho jaye ga
+        System.out.println(ab );
+    }
+}
